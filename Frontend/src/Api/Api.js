96167a16e09764804975488140f0f4cbea36d0a6
Kaifu-stack <kaif22101@gmail.com>
2025-11-07 11:39:22 +0530

    message

--- Frontend/src/Api/Api.js
+++ Frontend/src/Api/Api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://pcrms-z7gt.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
 });
 
